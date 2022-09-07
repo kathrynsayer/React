935397b94d8eee3ec7b19d776d7f86c6b01e3051
kathrynsayer <kssayer@gmail.com>
2022-09-07 16:46:48 -0700

    part 2

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import peanut from './peanut.png';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      list: ['ready', 'set', 'GO'],
+      text: "",
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    let newList = [...this.state.list, this.state.text];
+    this.setState({ list: newList, text: ""});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <form onSubmit={this.onSubmit}>
+        <input 
+        type="text" 
+        name="text" 
+        id="text" 
+        value={this.state.text} 
+        onChange={(event) => this.setState({ text: event.target.value})} 
+        />
+        <button type="submit">Add</button>
+        </form>
+        
+        <ul>
+        {this.state.list.map((item, idx) => {
+          return <li key={item + idx}>{item}</li>
+        })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
